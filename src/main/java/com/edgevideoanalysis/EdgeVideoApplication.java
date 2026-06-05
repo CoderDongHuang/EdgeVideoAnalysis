ea1953cb3ffcade8182c9dbc48ce36bf7dda971c
CoderDongHuang <1218798773@qq.com>
2026-06-05 23:49:44 +0800

    项目初始化

---
+++ src/main/java/com/edgevideoanalysis/EdgeVideoApplication.java
@@ -0,0 +1,12 @@
+package com.edgevideoanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeVideoApplication.class, args);
+    }
+}
