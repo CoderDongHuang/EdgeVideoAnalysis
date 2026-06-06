72844bd992bcf6d91168f0fda7d25fcfb9ece890
CoderDongHuang <1218798773@qq.com>
2026-06-06 15:06:12 +0800

    添加对应的DTO类，用于设备控制

---
+++ src/main/java/com/edgevideoanalysis/device/dto/LampControlDTO.java
@@ -0,0 +1,11 @@
+package com.edgevideoanalysis.device.dto;
+
+import lombok.Data;
+
+@Data
+public class LampControlDTO {
+
+    private Long lampId;
+
+    private String command;
+}
---
+++ src/main/java/com/edgevideoanalysis/device/dto/LampDTO.java
@@ -0,0 +1,15 @@
+package com.edgevideoanalysis.device.dto;
+
+import lombok.Data;
+
+@Data
+public class LampDTO {
+
+    private String lampCode;
+
+    private String lampName;
+
+    private String location;
+
+    private String cameraUrl;
+}
