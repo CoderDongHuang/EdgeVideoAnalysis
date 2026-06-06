4e1ed82e206ec6ed911c40c07e676b2a012b6326
CoderDongHuang <1218798773@qq.com>
2026-06-06 15:04:27 +0800

    添加对应的Mapper接口和XML文件

---
+++ src/main/java/com/edgevideoanalysis/device/mapper/LampMapper.java
@@ -0,0 +1,9 @@
+package com.edgevideoanalysis.device.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edgevideoanalysis.device.entity.Lamp;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LampMapper extends BaseMapper<Lamp> {
+}
