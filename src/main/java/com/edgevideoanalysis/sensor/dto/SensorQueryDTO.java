177380d93742d2edbc3a653ddf46ed3b4c8de206
CoderDongHuang <1218798773@qq.com>
2026-06-07 14:52:01 +0800

    GET 请求的时间参数必须加 @DateTimeFormat 注解，多次犯错了，记住了

--- src/main/java/com/edgevideoanalysis/sensor/dto/SensorQueryDTO.java
+++ src/main/java/com/edgevideoanalysis/sensor/dto/SensorQueryDTO.java
@@ -4,13 +4,17 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Data
 public class SensorQueryDTO {
 
     private Long lampId;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startTime;
-
+    
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime endTime;
 
     private String sensorType;
