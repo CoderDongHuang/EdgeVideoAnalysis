1c574a7ee0ffb008e7247eaab38fdc4953faa82d
CoderDongHuang <1218798773@qq.com>
2026-06-07 15:03:27 +0800

    实现报警mapper接口

---
+++ src/main/java/com/edgevideoanalysis/alarm/mapper/AlarmRuleMapper.java
@@ -0,0 +1,9 @@
+package com.edgevideoanalysis.alarm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edgevideoanalysis.alarm.entity.AlarmRule;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AlarmRuleMapper extends BaseMapper<AlarmRule> {
+}
